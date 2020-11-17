ba833041ed1668651bc0c54ef36d235629c00913
Steven Le Moal <steven.lemoal@gmail.com>
2020-11-17 18:03:57 +0100

    Added the functionality to add a dog as a user. Changed the link in the navbar to send to the correct page

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,9 +1,34 @@
 class DogsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     @dogs = Dog.all
   end
 
   def show
     @dog = Dog.find(params[:id])
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    @dog.owner = current_user
+    if @dog.save!
+      redirect_to dog_path(@dog)
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def dog_params
+    params.require(:dog).permit(:name, :size, :breed, :rate, :description, :picture)
+  end
+
+  def set_dog
+    @dog = Det.find(params[:id])
   end
 end
